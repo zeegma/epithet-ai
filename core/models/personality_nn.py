24e6b75f3b994a6150fca855bdd287dd208e5bc0
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-06-26 22:40:36 +0800

    Reduced HL & removed softmax activation

--- core/models/personality_nn.py
+++ core/models/personality_nn.py
@@ -3,28 +3,29 @@ import torch.nn as nn
 
 
 class PersonalityNN(nn.Module):
-    def __init__(self, input_size=15, hidden_size=32, output_size=8):
+    def __init__(self, input_size=15, hidden_size=16, output_size=8):
         super(PersonalityNN, self).__init__()
-        # Linear layer: 15 inpput features -> 32 hidden
+        # Linear layer: 15 inpput features -> 16 hidden
         self.fc1 = nn.Linear(input_size, hidden_size)
-        # Linear layer: 32 hidden -> 32 hidden
-        self.fc2 = nn.Linear(hidden_size, hidden_size)
-        # Output layer: 32 hidden -> 8 categories
-        self.fc3 = nn.Linear(hidden_size, output_size)
+        # Output layer: 16 hidden -> 8 categories
+        self.fc2 = nn.Linear(hidden_size, output_size)
         # Regularization technique to prevent overfitting
         self.dropout = nn.Dropout(0.3)
+        self.init_weights()
+
+    def init_weights(self):
+        # He initialization
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_uniform_(m.weight)
+                nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
         # Layer 1 + ReLU then apply dropout
         x = torch.relu(self.fc1(x))
         x = self.dropout(x)
 
-        # Layer 2 + ReLU then apply dropout
-        x = torch.relu(self.fc2(x))
-        x = self.dropout(x)
+        # Output layer
+        x = self.fc2(x)
 
-        # Output layer + Softmax
-        x = torch.softmax(self.fc3(x), dim=1)
-
-        # Then return softmax activated output
         return x
