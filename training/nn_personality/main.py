361b5bf07ab6d73883f40a62bdc89c0775f68f42
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-06-26 19:17:18 +0800

    Created main file for testing NN code

---
+++ training/nn_personality/main.py
@@ -0,0 +1,57 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+
+from training.nn_personality.train import train
+from core.predict.personality import predict_personality
+
+
+def do_train():
+    train()
+
+
+def do_predict():
+    # Sample quiz input
+    # Acelle
+    # answers = [4, 4, 2, 3, 2, 3, 4, 3, 1, 1, 2, 3, 4, 3, 1]
+
+    # Ti met
+    # answers = [2, 3, 3, 1, 1, 3, 1, 3, 1, 3, 1, 2, 4, 4, 1]
+
+    # Syruz Ken
+    # answers = [4, 3, 4, 1, 4, 2, 2, 2, 3, 1, 3, 2, 2, 4, 2]
+
+    # Chrysler
+    answers = [2, 3, 1, 1, 4, 4, 1, 1, 4, 1, 2, 3, 3, 4, 2]
+
+    # Fervicmar
+    # answers = [2, 4, 2, 3, 2, 3, 3, 2, 4, 1, 2, 2, 4, 3, 2]
+
+    # Henry
+    # answers = [2, 2, 2, 1, 3, 1, 4, 1, 3, 1, 2, 1, 3, 1, 2]
+
+    # Princess Jane
+    # answers = [4, 3, 2, 1, 2, 3, 3, 2, 3, 1, 2, 4, 2, 3, 2]
+
+    # Predict using imported predict function
+    _ = predict_personality(answers)
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("> Enter: python main.py [train|predict]")
+        return
+
+    command = sys.argv[1]
+
+    if command == "train":
+        do_train()
+    elif command == "predict":
+        do_predict()
+    else:
+        print(f"> Unknown command: {command}")
+
+
+if __name__ == "__main__":
+    main()
