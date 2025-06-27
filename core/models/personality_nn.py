e00261a14a095ac94a42e0f53a44433c504c8bd4
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-06-27 17:31:13 +0800

    Changed model architecture to use embeddings

--- core/models/personality_nn.py
+++ core/models/personality_nn.py
@@ -1,20 +1,32 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class PersonalityNN(nn.Module):
-    def __init__(self, input_size=15, hidden_size=[64, 32, 16], output_size=8):
+    def __init__(
+        self,
+        fan_in=15,
+        num_choices=5,
+        emb_dim=4,
+        hidden_size=[48, 24],
+        fan_out=8,
+    ):
         super(PersonalityNN, self).__init__()
-        # Linear layer: 15 inpput features -> 64 hidden
-        self.fc1 = nn.Linear(input_size, hidden_size[0])
-        # Linear layer: 64 hidden -> 32 hidden
-        self.fc2 = nn.Linear(hidden_size[0], hidden_size[1])
+        # 15 questions -> 15 embedding layers [shared size: 4]
+        self.embeddings = nn.ModuleList(
+            [nn.Embedding(num_choices, emb_dim) for _ in range(fan_in)]
+        )
+        # Calculate input dimensions [15 * 40 = 60]
+        input_dim = fan_in * emb_dim
+        # Linear layer: 15 inpput features -> 32 hidden
+        self.fc1 = nn.Linear(input_dim, hidden_size[0])
         # Linear layer: 32 hidden -> 16 hidden
-        self.fc3 = nn.Linear(hidden_size[1], hidden_size[2])
+        self.fc2 = nn.Linear(hidden_size[0], hidden_size[1])
         # Output layer: 16 hidden -> 8 categories
-        self.out = nn.Linear(hidden_size[2], output_size)
-        # Regularization technique to prevent overfitting
-        self.dropout = nn.Dropout(0.3)
+        self.out = nn.Linear(hidden_size[1], fan_out)
+        # Dropout to prevent overfitting
+        self.dropout = nn.Dropout(0.4)
         # Init weights using He
         self.init_weights()
 
@@ -26,16 +38,17 @@ class PersonalityNN(nn.Module):
                 nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
-        # Layer 1 + leaky ReLU then apply dropout
+        # X shape: (batch size, 15)
+        # Convert each column to an embedding then concatenate
+        embeds = [self.embeddings[i](x[:, i].long()) for i in range(x.shape[1])]
+        # shape: (batch size, 15 * emb_dim)
+        x = torch.cat(embeds, dim=1)
+
+        # Layer 1 + leaky ReLU then no dropout for first layer
         x = F.leaky_relu(self.fc1(x), negative_slope=0.01)
-        x = self.dropout(x)
 
-        # Layer 2 + leaky ReLU then apply dropout
+        # Layer 2 + leaky ReLU then apply dropout only on final HL
         x = F.leaky_relu(self.fc2(x), negative_slope=0.01)
-        x = self.dropout(x)
-
-        # Layer 3 + leaky ReLU then apply dropout
-        x = F.leaky_relu(self.fc3(x), negative_slope=0.01)
         x = self.dropout(x)
 
         # Output layer
